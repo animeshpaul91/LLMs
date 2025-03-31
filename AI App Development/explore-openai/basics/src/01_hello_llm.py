da2f2795286b24d68fe0b0064a86461f9dab3eac
Animesh Paul <ani.nitmz@gmail.com>
2025-03-30 21:43:28 -0700

    Cleaned code

--- AI App Development/explore-openai/basics/src/01_hello_llm.py
+++ AI App Development/explore-openai/basics/src/01_hello_llm.py
@@ -1,31 +1,38 @@
-import os
 import json
-from openai import OpenAI
+import os
+
 from dotenv import load_dotenv
+from openai import OpenAI
+from openai.types.chat.chat_completion import ChatCompletion
 
+# Set up load_dotenv()
+load_dotenv() # sets up env variables from .env
 
-if __name__ == "__main__":
-    # Set up load_dotenv()
-    load_dotenv() # sets up env variables from .env
+# Call open-ai model using chat completion create.
+client = OpenAI()
+LLM = os.environ.get("OPEN_AI_MODEL")
 
-    # Call open-ai model using chat completion create.
-    client = OpenAI()
-    LLM = os.environ.get("OPEN_AI_MODEL")
-
+def ask_openai(input_prompt: str) -> ChatCompletion:
     response = client.chat.completions.create(
         model=LLM,
         messages=[
             {
                 "role": "user",
-                "content": "What is an LLM ?"
+                "content": input_prompt
             }
         ],
     )
+    return response
 
+if __name__ == "__main__":
     # Print the Type and Response
-    print(f"Response type : {type(response)}")
+    user_question = "Generate the code for the sum of n numbers"
+    llm_response = ask_openai(user_question)
+    print(f"Response type : {type(llm_response)}")
 
     # Pretty print the entire response
-    # print(f"Response : {response.choices[0].message.content}")
-    response_dict = response.to_dict()
+    response_dict = llm_response.to_dict()
     print(json.dumps(response_dict, indent=4))
+
+    # Print the response text
+    print(f"Response : {llm_response.choices[0].message.content}")
