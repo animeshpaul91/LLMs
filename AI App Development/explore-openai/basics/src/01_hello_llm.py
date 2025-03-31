83fad09648cca235afb5e5caa1bd7996fa6ee0d9
Animesh Paul <ani.nitmz@gmail.com>
2025-03-30 21:32:36 -0700

    First Successful request made to OpenAI API

--- AI App Development/explore-openai/basics/src/01_hello_llm.py
+++ AI App Development/explore-openai/basics/src/01_hello_llm.py
@@ -1,9 +1,31 @@
-LLM = "gpt-4o-mini"
+import os
+import json
+from openai import OpenAI
+from dotenv import load_dotenv
 
-# Set up load_dotenv()
 
-# Call open-ai model using chat completion create.
+if __name__ == "__main__":
+    # Set up load_dotenv()
+    load_dotenv() # sets up env variables from .env
 
-# Print the Type and Response
+    # Call open-ai model using chat completion create.
+    client = OpenAI()
+    LLM = os.environ.get("OPEN_AI_MODEL")
 
-# Pretty print the entire response
+    response = client.chat.completions.create(
+        model=LLM,
+        messages=[
+            {
+                "role": "user",
+                "content": "What is an LLM ?"
+            }
+        ],
+    )
+
+    # Print the Type and Response
+    print(f"Response type : {type(response)}")
+
+    # Pretty print the entire response
+    # print(f"Response : {response.choices[0].message.content}")
+    response_dict = response.to_dict()
+    print(json.dumps(response_dict, indent=4))
