24470378ec0637db3977e44ef6c1a3c752d47a6e
Animesh Paul <ani.nitmz@gmail.com>
2025-03-31 21:06:19 -0700

    Added temparature

--- AI App Development/explore-openai/basics/src/02_openai_parameters.py
+++ AI App Development/explore-openai/basics/src/02_openai_parameters.py
@@ -11,25 +11,27 @@ LLM = os.environ.get("OPEN_AI_MODEL")
 
 
 # Call the openai chat.completions endpoint
-def ask_openai(
-    user_question: str,
-) -> ChatCompletion:
+def ask_openai(user_question: str, temperature: float = 1.0) -> ChatCompletion:
     response = client.chat.completions.create(
-        model="gpt-4o",
-        messages=[
-            {"role": "user", "content": user_question},
-        ],
+        model=LLM,
+        messages=[{"role": "user", "content": user_question}],
+        temperature=temperature
     )
 
-    print(f"response  type : {type(response)}")
+    # print(f"response  type : {type(response)}")
     return response
+
+
+def get_response(user_question: str, temperature: float = 0.2) -> str:
+    response = ask_openai(user_question, temperature)
+    return response.choices[0].message.content
 
 
 if __name__ == "__main__":
     # temperature
     # 0.2 -> Deterministic
     # 1 and above  -> non deterministic
-    user_question = """
+    input_prompt = """
         Can you complete the sentence ?
 
         My dog is playful and love to
@@ -39,5 +41,6 @@ if __name__ == "__main__":
     # Start with 1, -> run around in the park, chasing after balls and playing with other dogs.
     #  0.5-> My dog is playful and loves to chase after balls in the park.
     #  0.2-> My dog is playful and loves to chase after balls in the park.
-    response: ChatCompletion = ask_openai(user_question, top_p=0.2)
-    print(f"response_temp_01 : {response.choices[0].message.content}")
+
+    response_text: str = get_response(input_prompt, temperature=1.0)
+    print(f"response_temp_01 : {response_text}")
