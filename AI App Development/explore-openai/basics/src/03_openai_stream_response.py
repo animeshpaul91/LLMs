0a59394e8d1c1fc4c94f26225667484f9815545b
Animesh Paul <ani.nitmz@gmail.com>
2025-04-02 21:35:24 -0700

    Added Streaming

--- AI App Development/explore-openai/basics/src/03_openai_stream_response.py
+++ AI App Development/explore-openai/basics/src/03_openai_stream_response.py
@@ -17,7 +17,7 @@ def ask_openai(
     top_p: float = 1.0,
     max_tokens: int = 256,
 ) -> Stream[ChatCompletionChunk]:
-    print(f"LLM : {LLM}")
+    #print(f"LLM : {LLM}")
     response = client.chat.completions.create(
         model=LLM,
         messages=[
@@ -26,20 +26,20 @@ def ask_openai(
         temperature=temperature,
         max_tokens=max_tokens,
         top_p=top_p,
-        stream=True,
+        stream=True, # This will make the response of type Stream[ChatCompletionChunk]
     )
 
-    print(f"response  type : {type(response)}")
+    #print(f"response  type : {type(response)}")
     return response
 
 
 if __name__ == "__main__":
-    user_question = """
+    input_prompt = """
         Generate the content for creating a course on python programming.
         """
-    response: Stream[ChatCompletionChunk] = ask_openai(user_question)
-    # print(f"response_temp_01 : {response.choices[0].message.content}")
-    for chunk in response:
+    llm_response: Stream[ChatCompletionChunk] = ask_openai(input_prompt)
+    #print(f"response_temp_01 : {llm_response.choices[0].message.content}")
+    for chunk in llm_response:
         # Extract the chunk text
         chunk_message = chunk.choices[0].delta.content  # Access the content directly
         if chunk_message:
