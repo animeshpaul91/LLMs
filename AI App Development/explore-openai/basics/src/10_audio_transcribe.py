f8590c05f69c13a225752de0422a0f55cbedf4e9
Animesh Paul <ani.nitmz@gmail.com>
2025-04-20 21:30:04 -0700

    Revisited Transcription

--- AI App Development/explore-openai/basics/src/10_audio_transcribe.py
+++ AI App Development/explore-openai/basics/src/10_audio_transcribe.py
@@ -1,27 +1,33 @@
-import base64
 import os
 
 from dotenv import load_dotenv
 from openai import OpenAI
-from openai.types.chat.chat_completion import ChatCompletion
 from openai.resources.audio.transcriptions import Transcription
+
 load_dotenv()
 
 client = OpenAI()
 LLM = os.environ.get("OPEN_AI_MODEL")
 
 # Call the openai chat.completions endpoint
-def ask_openai(
-) :  
+def ask_openai(audio_file_path: str,
+               prompt: str = "") :
     print(f"LLM : {LLM}")
-    transcription = ""
-
+    file = open(audio_file_path, "rb")
+    transcription: Transcription = client.audio.transcriptions.create(
+        model="whisper-1",
+        file=file,
+        prompt=prompt
+    )
     return transcription
 
 
 if __name__ == "__main__":
-    
-    # audio_path = "src/resources/Taylor_Swift_Delicate.mp3"
-    audio_path = "src/resources/RAG.mp3"
-        # YT_Link -> https://www.youtube.com/watch?v=T-D1OfcDW1M&t=20s
-    # prompt = "This audio explains the concepts about RAG"
+    audio_path = "resources/Taylor_Swift_Delicate.mp3"
+    # audio_path = "resources/RAG.mp3"
+    # YT_Link -> https://www.youtube.com/watch?v=T-D1OfcDW1M&t=20s
+
+    input_prompt = "This audio explains the concepts about RAG"
+    response: Transcription = ask_openai(audio_file_path=audio_path, prompt=input_prompt)
+    transcription_text = response.text
+    print(f"Transcription_text : {transcription_text}")
