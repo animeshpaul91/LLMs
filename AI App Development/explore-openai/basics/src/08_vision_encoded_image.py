e856b41451351a942515cf1da30f4c431b40deac
Animesh Paul <ani.nitmz@gmail.com>
2025-04-20 20:04:42 -0700

    Vision API

--- AI App Development/explore-openai/basics/src/08_vision_encoded_image.py
+++ AI App Development/explore-openai/basics/src/08_vision_encoded_image.py
@@ -13,20 +13,16 @@ LLM = os.environ.get("OPEN_AI_MODEL")
 api_key = os.environ.get("OPENAI_API_KEY")
 
 
-# Function to encode the image
+# Returns a base64 encoded string for an image
 def encode_image(image_path):
     with open(image_path, "rb") as image_file:
         return base64.b64encode(image_file.read()).decode("utf-8")
 
 
-image_path = "src/resources/invoice-template.png"
-
-base64_image = encode_image(image_path)
-
-
 # Call the openai chat.completions endpoint
 def ask_openai(
     user_question: str,
+    base64_image_string: str,
     temperature: float = 1.0,
     top_p: float = 1.0,
     max_tokens: int = 500,
@@ -36,7 +32,7 @@ def ask_openai(
     headers = {"Content-Type": "application/json", "Authorization": f"Bearer {api_key}"}
 
     payload = {
-        "model": "gpt-4o-mini",
+        "model": LLM,
         "messages": [
             {
                 "role": "user",
@@ -44,7 +40,7 @@ def ask_openai(
                     {"type": "text", "text": user_question},
                     {
                         "type": "image_url",
-                        "image_url": {"url": f"data:image/jpeg;base64,{base64_image}"},
+                        "image_url": {"url": f"data:image/jpeg;base64,{base64_image_string}"},
                     },
                 ],
             }
@@ -62,15 +58,18 @@ def ask_openai(
 
 
 if __name__ == "__main__":
-    # Step 4 :
-    user_question = """Hello, Extract the info from this invoice image in json format.
+    # Get Base64 Encoded string version of the image
+    file_path = "resources/invoice-template.png"
+    base64_image = encode_image(file_path)
+
+    llm_prompt = """Hello, Extract the info from this invoice image in json format.
     Do not include ```json in the response.
     """
 
-    response = ask_openai(user_question)
+    llm_response = ask_openai(llm_prompt, base64_image)
 
-# Directly parse the response and pretty-print the JSON in one step
-    clean_content_json = json.dumps(
-        json.loads(response.json()["choices"][0]["message"]["content"]), indent=4
-    )
-    print(clean_content_json)
+    # Directly parse the response and pretty-print the JSON in one step
+    clean_content_json = json.loads(llm_response.json()["choices"][0]["message"]["content"])
+    clean_content_json = json.dumps(clean_content_json, indent=4)
+
+    print(f"Response: {clean_content_json}")
