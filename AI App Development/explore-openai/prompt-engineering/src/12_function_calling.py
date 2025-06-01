0e5688cf0b5dcb601f0f6afee728e025d5af54f3
Animesh Paul <ani.nitmz@gmail.com>
2025-05-31 22:09:38 -0700

    Created Re-usable looping App

--- AI App Development/explore-openai/prompt-engineering/src/12_function_calling.py
+++ AI App Development/explore-openai/prompt-engineering/src/12_function_calling.py
@@ -95,7 +95,7 @@ def ask_openai(
 ) -> ChatCompletion:
     messages = [{"role": "system", "content": system_message}]
     messages.extend(custom_messages)
-    print(f"messages : {messages}")
+    # print(f"messages : {messages}")
     response = client.chat.completions.create(
         model="gpt-4o",
         messages=messages,
@@ -104,36 +104,35 @@ def ask_openai(
     )
     return response
 
+def app() -> None:
+    while True:
+        prompt = input("Enter your input prompt. Enter 'exit' to quit: \n")
+
+        if prompt == "exit":
+            break
+
+        user_message = [{"role": "user", "content": prompt}]
+        prompt_response: ChatCompletion = ask_openai(user_message)
+        message = prompt_response.choices[0].message
+        print(f"LLM Response: {message.content}")
+
+        if message.tool_calls:
+            function = message.tool_calls[0].function
+            # print(f"function : {function}")
+            tool_name = function.name
+            if tool_name == "get_current_time":
+                result = get_current_time()
+                tool_call_id = message.tool_calls[0].id
+                custom_messages = [
+                    {"role": "user", "content": prompt},
+                    message,
+                    {"role": "tool", "name": tool_name, "content": result, "tool_call_id": tool_call_id}
+                ]
+                second_response = ask_openai(custom_messages=custom_messages)
+                print(f"Assistant Response from Tool: {second_response.choices[0].message.content}")
+            else:
+                print(f"Assistant Response : {message.content}")
+
 
 if __name__ == "__main__":
-    # Initial prompt
-    #prompt = "Whats the current time?"
-    #prompt = "Whats the current weather in new york?"
-    prompt = "Whats the current stock value of Tesla?"
-
-    user_message = [{"role": "user", "content": prompt}]
-    prompt_response: ChatCompletion = ask_openai(user_message)
-    message = prompt_response.choices[0].message
-    print(f"Assistant Response : {message}")
-
-    if message.tool_calls:
-        function = message.tool_calls[0].function
-        print(f"function : {function}")
-        tool_name = function.name
-        if tool_name == "get_current_time":
-            result = get_current_time()
-            tool_call_id = message.tool_calls[0].id
-            custom_messages = [
-                {"role": "user", "content": prompt},
-                message,
-                {"role": "tool", "name": tool_name, "content": result, "tool_call_id": tool_call_id}
-            ]
-            second_response = ask_openai(custom_messages=custom_messages)
-            print(f"Assistant Response from Tool: {second_response.choices[0].message.content}")
-        else:
-            print(f"Assistant Response : {message.content}")
-    else:
-        print("Tool not available with LLM")
-
-    # Complete App
-    # app()
+    app()
