8d71be9581c070ac4ff80e9bc9f1b1f5bdb90326
Animesh Paul <ani.nitmz@gmail.com>
2025-07-04 21:02:15 -0700

    Add simple chatbot

--- AI App Development/explore-openai/streamlit-chat-ui/src/1_app.py
+++ AI App Development/explore-openai/streamlit-chat-ui/src/1_app.py
@@ -1,8 +1,8 @@
 import os
 
+import streamlit as st
 from dotenv import load_dotenv
 from openai import OpenAI
-from openai.types.chat.chat_completion import ChatCompletion
 
 load_dotenv()
 
@@ -11,22 +11,31 @@ LLM = os.environ.get("OPEN_AI_MODEL")
 
 
 # Call the openai chat.completions endpoint
-def ask_openai(
-    user_question: str,
-    temperature: float = 1.0,
-    top_p: float = 1.0,
-    max_tokens: int = 256,
-    stream=False,
-) -> ChatCompletion:
-    response = client.chat.completions.create(
-        model=LLM,
-        messages=[
-            {"role": "user", "content": user_question},
-        ],
+def ask_openai(user_question: str, temperature: float = 1.0, top_p: float = 1.0, max_tokens: int = 256, stream=False) -> str:
+    print("Starting LLM call")
+    response = client.chat.completions.create(model=LLM,
+        messages=[{"role": "user", "content": user_question}],
         temperature=temperature,
         max_tokens=max_tokens,
         top_p=top_p,
-        stream=stream,
+        #stream=stream
     )
 
-    return response
+    llm_response = response.choices[0].message.content
+    print(f"Fetched LLM response: \n{llm_response}")
+    return llm_response
+
+st.set_page_config(page_title="Chat Application")
+st.header("Chat :blue[Application]")
+st.chat_message("ai").write("Hello, how can I help you today?")
+
+def run() -> None:
+    prompt = st.chat_input("Add your prompt..")
+
+    if prompt:
+        st.chat_message("human").write(prompt)
+        llm_output: str = ask_openai(prompt)
+        st.chat_message("ai").write(llm_output)
+
+if __name__ == "__main__":
+    run()
