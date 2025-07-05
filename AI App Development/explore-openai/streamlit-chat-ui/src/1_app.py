62b08b0827a62108d35f9f9e53e0632645692ff3
Animesh Paul <ani.nitmz@gmail.com>
2025-07-04 21:12:52 -0700

    Format

--- AI App Development/explore-openai/streamlit-chat-ui/src/1_app.py
+++ AI App Development/explore-openai/streamlit-chat-ui/src/1_app.py
@@ -2,6 +2,7 @@ import os
 
 import streamlit as st
 from dotenv import load_dotenv
+from jeepney.low_level import Boolean
 from openai import OpenAI
 
 load_dotenv()
@@ -11,19 +12,21 @@ LLM = os.environ.get("OPEN_AI_MODEL")
 
 
 # Call the openai chat.completions endpoint
-def ask_openai(user_question: str, temperature: float = 1.0, top_p: float = 1.0, max_tokens: int = 256, stream=False) -> str:
+def ask_openai(user_question: str, temperature: float = 1.0, top_p: float = 1.0, max_tokens: int = 256,
+               stream: Boolean = False) -> str:
     print("\nStarting LLM call")
     response = client.chat.completions.create(model=LLM,
-        messages=[{"role": "user", "content": user_question}],
-        temperature=temperature,
-        max_tokens=max_tokens,
-        top_p=top_p,
-        #stream=stream
-    )
+                                              messages=[{"role": "user", "content": user_question}],
+                                              temperature=temperature,
+                                              max_tokens=max_tokens,
+                                              top_p=top_p,
+                                              stream=stream
+                                              )
 
     llm_response = response.choices[0].message.content
     print(f"Fetched LLM response: \n{llm_response}")
     return llm_response
+
 
 def set_streamlit_config():
     st.set_page_config(page_title="Chat Application")
@@ -38,6 +41,7 @@ def run() -> None:
         st.chat_message("human").write(prompt)
         llm_output: str = ask_openai(prompt)
         st.chat_message("ai").write(llm_output)
+
 
 if __name__ == "__main__":
     set_streamlit_config()
